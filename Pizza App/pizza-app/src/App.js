7eeb9646e9e09ecbf766776daf34dd2cc6ca66d2
Courtney Dart <dart.courtney1@gmail.com>
2020-12-19 00:44:10 -0800

    Created Total Card footer, created Veggies checkboxes and logic to set and change state. Tested and working correctly.

--- Pizza App/pizza-app/src/App.js
+++ Pizza App/pizza-app/src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import PizzaForm from "./components/Forms";
 
 function App() {
-  // States
+  // Size
   const [selectedSize, setSelectedSize] = useState("Personal");
-  const [selectedSizeCost, setSelectedSizeCost] = useState(0);
+  const [selectedSizeCost, setSelectedSizeCost] = useState(6);
+
+  // Crust
   const [selectedCrust, setSelectedCrust] = useState("Plain Crust");
   const [selectedCrustCost, setSelectedCrustCost] = useState(0);
+
+  // Veggies
+  const [allVeggies, setAllVeggies] = useState([
+    {vegName : "Tomatoes", isChecked : false},
+    {vegName : "Onions", isChecked : false},
+    {vegName : "Olives", isChecked : false},
+    {vegName : "Green Peppers", isChecked : false},
+    {vegName : "Mushrooms", isChecked : false},
+    {vegName : "Pineapple", isChecked : false},
+    {vegName : "Spinach", isChecked : false},
+    {vegName : "Jalapeno", isChecked : false},
+  ]);
+  const [selectedVegCost, setSelectedVegCost] = useState(0);
   const [selectedVeggies, setSelectedVeggies] = useState([]);
+
+  // Order total
   const [orderPlaced, setOrderPlaced] = useState(false);
+  const [orderTotal, setOrderTotal] = useState();
 
   return (
     <div className="App">
@@ -25,8 +43,16 @@ function App() {
         setSelectedCrust={setSelectedCrust}
         selectedCrustCost={selectedCrustCost}
         setSelectedCrustCost={setSelectedCrustCost}
+        allVeggies={allVeggies}
+        setAllVeggies={setAllVeggies}
+        selectedVegCost={selectedVegCost}
+        setSelectedVegCost={setSelectedVegCost}
+        selectedVeggies={selectedVeggies}
+        setSelectedVeggies={setSelectedVeggies}
         orderPlaced={orderPlaced}
         setOrderPlaced={setOrderPlaced}
+        orderTotal={orderTotal}
+        setOrderTotal={setOrderTotal}
       />
     </div>
   );
--- Pizza App/pizza-app/src/components/Forms.js
+++ Pizza App/pizza-app/src/components/Forms.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Form } from 'react-bootstrap';
 import { CardGroup } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
@@ -9,51 +9,122 @@ const PizzaForm = ({ selectedSize, setSelectedSize,
                     selectedSizeCost, setSelectedSizeCost, 
                     selectedCrust, setSelectedCrust,
                     selectedCrustCost, setSelectedCrustCost, 
-                    orderPlaced, setOrderPlaced }) => {
+                    allVeggies, setAllVeggies,
+                    selectedVeggies, setSelectedVeggies,
+                    selectedVegCost, setSelectedVegCost,
+                    orderPlaced, setOrderPlaced,
+                    orderTotal, setOrderTotal }) => {
 
     // Sets selected size state.
     const selectedSizeHandler = (e) => {
-        setSelectedSize(e.target.value);
-    }
-
-    //   Sets selected crust state.
-    const selectedCrustHandler = (e) => {
-        setSelectedCrust(e.target.value)
-    }
-
-    // set costs of each selected item 
-    const getCosts = (e) => {
         const sizeCosts = {
             "Personal": 6, 
             "Medium": 10,
             "Large": 14,
             "Extra Large": 16,
-          };
-          const crustCosts = {
+        };
+        setSelectedSize(e.target.value);
+        setSelectedSizeCost(sizeCosts[e.target.value]);
+    }
+
+    //   Sets selected crust state.
+    const selectedCrustHandler = (e) => {
+        const crustCosts = {
             "Plain Crust": 0,
             "Garlic Butter Crust": 2,
             "Cheese Stuffed Crust": 3,
             "Spicy Crust": 1,
             "House Special Crust": 0,
         };
-        e.preventDefault();
-        setSelectedSizeCost(sizeCosts[selectedSize])
-        setSelectedCrustCost(crustCosts[selectedCrust])
-        setOrderPlaced(true);
-    };
-
-    let receipt;
-    if (orderPlaced) {
-        receipt = <Receipt 
-        selectedSize={selectedSize}
-        selectedSizeCost={selectedSizeCost}
-        selectedCrust={selectedCrust}
-        selectedCrustCost={selectedCrustCost}
-        />
+        setSelectedCrust(e.target.value);
+        setSelectedCrustCost(crustCosts[e.target.value]);
     }
 
+    // veggies
+    const selectedVeggiesHandler = (e) => {
+        setAllVeggies(allVeggies.map((item) => {
+            if (item.vegName === e.target.value) {
+                return {
+                    /*With spreading, you can change a property non-destructively: 
+                    You make a copy of the object where the property has a different value. */
+                    ...item, isChecked: !item.isChecked
+                }
+            }
+            return item;
+        }));
+    }
+
+    useEffect(() => {
+        createCheckedVegList();
+    }, [allVeggies]);
+
+    useEffect(() => {
+        addUpVegCost();
+    }, [selectedVeggies]);
+
+    // add or remove veggies to list
+    const createCheckedVegList = () => {
+        setSelectedVeggies(allVeggies.filter((el) => el.isChecked === true));
+    }
+
+    const addUpVegCost = () => {
+        if (selectedVeggies.length !== 0) {
+            setSelectedVegCost(selectedVeggies.length - 1);
+        }
+    }
+
+    // Sets selected veggies state.
+    // const selectedVeggiesHandler = (e) => {
+    //     !selectedVeggies
+    //     setSelectedVeggies()
+    //     let count = 0;
+    // }
+    
+    //  useEffect is constantly listening for state changes, and it runs whenever it does change.
+    useEffect(() => {
+        orderTotalHandler();
+    }, [selectedSizeCost, selectedCrustCost, selectedVegCost]);
+
+    const orderTotalHandler = () => {
+        let total = selectedSizeCost + selectedCrustCost + selectedVegCost;
+        setOrderTotal(total);
+    }
+
+ 
+
+    // set costs of each selected item 
+    // const getCosts = (e) => {
+    //     const sizeCosts = {
+    //         "Personal": 6, 
+    //         "Medium": 10,
+    //         "Large": 14,
+    //         "Extra Large": 16,
+    //     };
+    //       const crustCosts = {
+    //         "Plain Crust": 0,
+    //         "Garlic Butter Crust": 2,
+    //         "Cheese Stuffed Crust": 3,
+    //         "Spicy Crust": 1,
+    //         "House Special Crust": 0,
+    //     };
+    //     e.preventDefault();
+    //     setSelectedSizeCost(sizeCosts[selectedSize])
+    //     setSelectedCrustCost(crustCosts[selectedCrust])
+    //     setOrderPlaced(true);
+    // };
+
+    // let receipt;
+    // if (orderPlaced) {
+    //     receipt = <Receipt 
+    //     selectedSize={selectedSize}
+    //     selectedSizeCost={selectedSizeCost}
+    //     selectedCrust={selectedCrust}
+    //     selectedCrustCost={selectedCrustCost}
+    //     />
+    // }
+
     return (
-        <div>
+        
              <Form>
             <CardGroup>
                 {/* Size Options */}
@@ -99,6 +170,7 @@ const PizzaForm = ({ selectedSize, setSelectedSize,
                             onChange={selectedSizeHandler}
                         />
                     </Card.Body>
+                    <Card.Footer className="text-danger"><h4>${selectedSizeCost}.00</h4></Card.Footer>
                 </Card>
                 {/* Crust Options */}
                 <Card
@@ -151,14 +223,89 @@ const PizzaForm = ({ selectedSize, setSelectedSize,
                             onChange={selectedCrustHandler}
                         />
                     </Card.Body>
+                    <Card.Footer className="text-danger"><h4>${selectedCrustCost}.00</h4></Card.Footer>
+                </Card>
+                {/* Veggie Options */}
+                <Card
+                    bg='warning'
+                    text='danger'
+                >
+                    <Card.Header>VEGGIES</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Select veggies:</Card.Title>
+                        <Card.Text>- You can choose more than one</Card.Text>
+                        {/* Size options radio */}
+                        <Form.Check
+                            type="checkbox"
+                            label="Tomatoes"
+                            name="veggies"
+                            value="Tomatoes"
+                            checked={allVeggies[0].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                        <Form.Check
+                            type="checkbox"
+                            label="Onions"
+                            name="veggies"
+                            value="Onions"
+                            checked={allVeggies[1].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                        <Form.Check
+                            type="checkbox"
+                            label="Olives"
+                            name="veggies"
+                            value="Olives"
+                            checked={allVeggies[2].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                        <Form.Check
+                            type="checkbox"
+                            label="Green Peppers"
+                            name="veggies"
+                            value="Green Peppers"
+                            checked={allVeggies[3].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                        <Form.Check
+                            type="checkbox"
+                            label="Mushrooms"
+                            name="veggies"
+                            value="Mushrooms"
+                            checked={allVeggies[4].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                        <Form.Check
+                            type="checkbox"
+                            label="Pineapple"
+                            name="veggies"
+                            value="Pineapple"
+                            checked={allVeggies[5].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                        <Form.Check
+                            type="checkbox"
+                            label="Spinach"
+                            name="veggies"
+                            value="Spinach"
+                            checked={allVeggies[6].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                        <Form.Check
+                            type="checkbox"
+                            label="Jalapeno"
+                            name="veggies"
+                            value="Jalapeno"
+                            checked={allVeggies[7].isChecked}
+                            onClick={selectedVeggiesHandler}
+                        />
+                    </Card.Body>
+                    <Card.Footer className="text-danger"><h4>${selectedVegCost}.00</h4></Card.Footer>
                 </Card>
             </CardGroup>
+            <Card.Footer className="text-danger"><h4>Total: ${orderTotal}.00</h4></Card.Footer>
         </Form>
-        <Button type="submit" onClick={getCosts}>Submit</Button>
-        <div>
-            {receipt}
-        </div>
-    </div>
+       
     );
 };
 
