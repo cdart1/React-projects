1c6c1e51dfdd0e5f383026361228cac863045128
Courtney Dart <dart.courtney1@gmail.com>
2020-12-18 17:41:14 -0800

    Updated form with two card components. Added bootstarp styling. Added logic to render a receipt component. Creatd Receipt component as child component of the From component.

--- Pizza App/pizza-app/src/App.js
+++ Pizza App/pizza-app/src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import PizzaForm from "./components/Forms";
 
 function App() {
+  // States
+  const [selectedSize, setSelectedSize] = useState("Personal");
+  const [selectedSizeCost, setSelectedSizeCost] = useState(0);
+  const [selectedCrust, setSelectedCrust] = useState("Plain Crust");
+  const [selectedCrustCost, setSelectedCrustCost] = useState(0);
+  const [selectedVeggies, setSelectedVeggies] = useState([]);
+  const [orderPlaced, setOrderPlaced] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome Vegan Pizza Lovers</h1>
       </header>
-      <PizzaForm />
+      <PizzaForm 
+        selectedSize={selectedSize}
+        setSelectedSize={setSelectedSize}
+        selectedSizeCost={selectedSizeCost}
+        setSelectedSizeCost={setSelectedSizeCost}
+        selectedCrust={selectedCrust}
+        setSelectedCrust={setSelectedCrust}
+        selectedCrustCost={selectedCrustCost}
+        setSelectedCrustCost={setSelectedCrustCost}
+        orderPlaced={orderPlaced}
+        setOrderPlaced={setOrderPlaced}
+      />
     </div>
   );
 }
--- Pizza App/pizza-app/src/components/Forms.js
+++ Pizza App/pizza-app/src/components/Forms.js
@@ -2,14 +2,67 @@ import React from "react";
 import { Form } from 'react-bootstrap';
 import { CardGroup } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import Receipt from "./Receipt";
 
-const PizzaForm = () => {
+const PizzaForm = ({ selectedSize, setSelectedSize, 
+                    selectedSizeCost, setSelectedSizeCost, 
+                    selectedCrust, setSelectedCrust,
+                    selectedCrustCost, setSelectedCrustCost, 
+                    orderPlaced, setOrderPlaced }) => {
+
+    // Sets selected size state.
+    const selectedSizeHandler = (e) => {
+        setSelectedSize(e.target.value);
+    }
+
+    //   Sets selected crust state.
+    const selectedCrustHandler = (e) => {
+        setSelectedCrust(e.target.value)
+    }
+
+    // set costs of each selected item 
+    const getCosts = (e) => {
+        const sizeCosts = {
+            "Personal": 6, 
+            "Medium": 10,
+            "Large": 14,
+            "Extra Large": 16,
+          };
+          const crustCosts = {
+            "Plain Crust": 0,
+            "Garlic Butter Crust": 2,
+            "Cheese Stuffed Crust": 3,
+            "Spicy Crust": 1,
+            "House Special Crust": 0,
+        };
+        e.preventDefault();
+        setSelectedSizeCost(sizeCosts[selectedSize])
+        setSelectedCrustCost(crustCosts[selectedCrust])
+        setOrderPlaced(true);
+    };
+
+    let receipt;
+    if (orderPlaced) {
+        receipt = <Receipt 
+        selectedSize={selectedSize}
+        selectedSizeCost={selectedSizeCost}
+        selectedCrust={selectedCrust}
+        selectedCrustCost={selectedCrustCost}
+        />
+    }
+
     return (
-        <Form>
+        <div>
+             <Form>
             <CardGroup>
-                <Card border='warning' style={{ width: '18rem' }}>
-                    <Card.Header>Slice</Card.Header>
-                    <Card.Body text='danger'>
+                {/* Size Options */}
+                <Card
+                    bg='warning'
+                    text='danger'
+                >
+                    <Card.Header>SLICE</Card.Header>
+                    <Card.Body>
                         <Card.Title>Select a size:</Card.Title>
                         <Card.Text>- Choose one</Card.Text>
                         {/* Size options radio */}
@@ -17,30 +70,95 @@ const PizzaForm = () => {
                             type="radio"
                             label="Personal"
                             name="size"
-                            value="6"
+                            value="Personal"
+                            checked={selectedSize === "Personal"}
+                            onChange={selectedSizeHandler} 
                         />
                         <Form.Check
                             type="radio"
                             label="Medium"
                             name="size"
-                            value="10"
+                            value="Medium"
+                            checked={selectedSize === "Medium"}
+                            onChange={selectedSizeHandler}
                         />
                         <Form.Check
                             type="radio"
                             label="Large"
                             name="size"
-                            value="10"
+                            value="Large"
+                            checked={selectedSize === "Large"}
+                            onChange={selectedSizeHandler}
                         />
                         <Form.Check
                             type="radio"
                             label="Extra Large"
                             name="size"
-                            value="10"
+                            value="Extra Large"
+                            checked={selectedSize === "Extra Large"}
+                            onChange={selectedSizeHandler}
+                        />
+                    </Card.Body>
+                </Card>
+                {/* Crust Options */}
+                <Card
+                    bg='warning'
+                    text='danger'
+                >
+                    <Card.Header>CRUST</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Select a crust:</Card.Title>
+                        <Card.Text>- Choose one</Card.Text>
+                        {/* Size options radio */}
+                        <Form.Check
+                            type="radio"
+                            label="Plain Crust"
+                            name="crust"
+                            value="Plain Crust"
+                            checked={selectedCrust === "Plain Crust"}
+                            onChange={selectedCrustHandler}
+                        />
+                        <Form.Check
+                            type="radio"
+                            label="Garlic Butter Crust"
+                            name="crust"
+                            value="Garlic Butter Crust"
+                            checked={selectedCrust === "Garlic Butter Crust"}
+                            onChange={selectedCrustHandler}
+                        />
+                        <Form.Check
+                            type="radio"
+                            label="Cheese Stuffed Crust"
+                            name="crust"
+                            value="Cheese Stuffed Crust"
+                            checked={selectedCrust === "Cheese Stuffed Crust"}
+                            onChange={selectedCrustHandler}
+                        />
+                        <Form.Check
+                            type="radio"
+                            label="Spicy Crust"
+                            name="crust"
+                            value="Spicy Crust"
+                            checked={selectedCrust === "Spicy Crust"}
+                            onChange={selectedCrustHandler}
+                        />
+                        <Form.Check
+                            type="radio"
+                            label="House Special Crust"
+                            name="crust"
+                            value="House Special Crust"
+                            checked={selectedCrust === "House Special Crust"}
+                            onChange={selectedCrustHandler}
                         />
                     </Card.Body>
                 </Card>
             </CardGroup>
         </Form>
+        <Button type="submit" onClick={getCosts}>Submit</Button>
+        <div>
+            {receipt}
+        </div>
+    </div>
     );
 };
 
